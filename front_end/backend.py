01a86f1fdff160017e99587135c84712976200fe
linksta7 <linksta7@gmail.com>
2019-11-09 16:29:26 -0500

    skeleton of backend created

--- front_end/backend.py
+++ front_end/backend.py
@@ -1,20 +1,91 @@
-Account_List = [1234567,7654321,1357246,6427531]
-Account_Balance = {}
-Account_Name = {"BobRoss123", }
+### TEST CODE ###
 
-#Method sorts code
-
-
-
-
-
-# infile = open("masterFile.txt", "r+")
+# Account_List = [1234567,7654321,1357246,6427531]
+# Account_Balance = {}
+# Account_Name = {"BobRoss123", }
+#
+# Accounts_Dict = {1234567: {'balance': 5000, 'name': 'bobross'},
+#                  7654321: {'balance': 5000, 'name': 'johndoe'}}
+#
+# #Method sorts code
+#
+# Accounts_Dict[7777777] = {'balance': 7, 'name': 'seven'}
 
 # Set all accounts to default balance of 5000
-for i in range(len(Account_List)):
-    Account_Balance[Account_List[i]] = 5000
+# for i in range(len(Account_List)):
+#     Account_Balance[Account_List[i]] = 5000
+# print(Account_Balance)
 
-print(Account_Balance)
+# print("Your account balance is:", Account_Balance)
+# print("Your account dictionary is:", Accounts_Dict)
+
+### Program starts here ###
+# opens master account file
+MAF_in = open("new_MAF.txt", "r+")
+MAF_lines = MAF_in.read().splitlines()
+# print("my infile is", MAF_lines)
+
+# creates and organizes dictionary for MAF
+real_Dict = {}
+for row in MAF_lines:
+    acc_num, balance, name = row.split()
+    real_Dict[acc_num] = {'balance': balance, 'name': name}
+
+
+# print("real dictionary is,", real_Dict)
+
+
+# transaction functions
+def deposit():
+    print("in deposit function")
+    return 0
+
+
+def withdraw():
+    print("in withdraw function")
+    return 0
+
+
+def transfer():
+    print("in transfer function")
+    return 0
+
+
+def create_acc():
+    print("go to create function")
+    return 0
+
+
+def delete_acc():
+    print("go to delete function")
+    return 0
+
+
+def end_sesh():
+    print("end of system")
+    return 0
+
+
+# Transaction checks start here
+TSF_in = open("TSF.txt", "r+")
+TSF_lines = TSF_in.read().splitlines()
+for row in TSF_lines:
+    ts_type, acc1, amount, acc2, acc_name = row.split()
+    print("my prints are", ts_type, acc1, amount, acc2, acc_num)
+    if ts_type == 'DEP':
+        deposit()
+    if ts_type == "WDR":
+        withdraw()
+    if ts_type == 'XFR':
+        transfer()
+    if ts_type == 'NEW':
+        create_acc()
+    if ts_type == 'DEL':
+        delete_acc()
+    if ts_type == 'EOS':
+        end_sesh()
+
 
 def dog():
     print("YOUR GAY")
+    # Your grammar is wrong ya clown
