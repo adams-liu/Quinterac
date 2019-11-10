6bfd77c9f2fc2dbf9ec87ffb682a92aa72769ad3
linksta7 <linksta7@gmail.com>
2019-11-09 19:16:40 -0500

    initial test

--- front_end/backend.py
+++ front_end/backend.py
@@ -1,10 +1,6 @@
-
-
-### Program starts here ###
 # opens master account file
 MAF_in = open("new_MAF.txt", "r+")
 MAF_lines = MAF_in.read().splitlines()
-# print("my infile is", MAF_lines)
 
 # creates and organizes dictionary for MAF
 real_Dict = {}
@@ -13,58 +9,69 @@ for row in MAF_lines:
     real_Dict[acc_num] = {'balance': int(balance), 'name': name}
 
 
-
-# transaction functions
+# transaction deposit
 def deposit(acc1,amount):
     real_Dict[acc1]["balance"] = real_Dict[acc1]["balance"] + amount
-    print("Final Amount" , real_Dict[acc1]["balance"])
     return 0
 
-
+# transaction withdraw
 def withdraw(acc1,amount):
     if(real_Dict[acc1]["balance"] >= amount):
         real_Dict[acc1]["balance"] = real_Dict[acc1]["balance"] - amount
-        print("Final Amount" , real_Dict[acc1]["balance"])
     else:
-        print("FATAL ERROR: Withdraw insufficient funds")
+        print("FATAL ERROR: withdraw insufficient funds")
     return 0
 
-
+# transaction transfer
 def transfer(acc1, amount, acc2):
     if real_Dict[acc2]["balance"] < amount:
         print("FATAL ERROR: transfer insufficient funds")
         return 0
     else:
         real_Dict[acc1]["balance"] += amount
+        real_Dict[acc2]["balance"] -= amount
     return 0
 
-
+# transaction create account
 def create_acc(acc, acc_name):
     real_Dict[acc] = {'balance': 500000, 'name': acc_name}
     return 0
 
-
+# transaction delete account
 def delete_acc(acc, acc_name):
-    if real_Dict[acc]['name'] != acc_name:
+    if (real_Dict[acc]['name'] != acc_name) and (real_Dict[acc]['balance'] != 0):
+        print("FATAL ERROR: account name does not match and account balance is not zero")
+    elif real_Dict[acc]['name'] != acc_name:
         print("FATAL ERROR: account name does not match")
+    elif real_Dict[acc]['balance'] != 0:
+        print("FATAL ERROR: account balance is not zero")
     else:
         del real_Dict[acc]
     return 0
 
-
+# transaction end of session
 def end_sesh():
-    print("end of system")
+    # store MAF lines
     new_MAF = []
-    print("Sorted dict")
+    # creates new account list with new MAF
+    acc_list_out = open("Account_List.txt", 'w')
+    # sorts dictionary keys
     for i in sorted(real_Dict.keys()):
+        # creates line for MAF
         new_MAF.append(i + " " + str(real_Dict[i]["balance"]) + " " + real_Dict[i]["name"] + "\n")
-        print(i, real_Dict[i])
+        # writes the (ordered) account number
+        acc_list_out.write(i)
+        acc_list_out.write("\n")
+    # closes new account list
+    acc_list_out.close()
 
-    outfile = open("newNEW_MAF.txt", 'w')
+    # creates new MAF file
+    MAF_out = open("new_MAF.txt", 'w')
     for i in new_MAF:
-        ## Add all contents of the list onto the actual transaction summary file
-        outfile.writelines(i)
-    outfile.close()
+        ## Add all contents of MAF list to MAF file
+        MAF_out.writelines(i)
+    # closes new MAF file
+    MAF_out.close()
     return 0
 
 
@@ -72,10 +79,11 @@ def end_sesh():
 TSF_in = open("TSF.txt", "r+")
 TSF_lines = TSF_in.read().splitlines()
 
+# Goes through each transaction line by line
 for row in TSF_lines:
     ts_type, acc1, amount, acc2, acc_name = row.split()
     amount = int(amount)
-
+    # Branches off to transaction function type depending on first attribute
     if ts_type == 'DEP':
         deposit(acc1,amount)
     if ts_type == "WDR":
